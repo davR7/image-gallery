78afc94d2d4919d5330e057fec552754d67781bd
Davidson Ratis <davidsonmakarios@gmail.com>
2020-11-08 11:27:11 -0300

    creation of image gallery

--- src/components/views/Gallery.vue
+++ src/components/views/Gallery.vue
@@ -1,13 +1,46 @@
 <template>
-  <div class="gallery"></div>
+  <div class="gallery">
+    <b-container>
+      <h2>Galeria de Imagem</h2>
+      <hr>
+      <div class="menu">
+        <b-button class="btn m-1">Todas Imagens</b-button>
+        <b-button class="btn m-1">Camera</b-button>
+        <b-button class="btn m-1">Fone de Ouvido</b-button>
+        <b-button class="btn m-1">SmartFone</b-button>
+        <b-button class="btn m-1">Tênis</b-button>
+        <b-button class="btn m-1">Relogio</b-button>
+      </div>
+      <div class="images">
+        <div class="row">
+          <ImgBox v-for="image in imgData" :key="image.id" :image="image" />
+        </div>
+      </div>
+    </b-container>
+  </div>
 </template>
 
 <script>
-export default {
+import ImgBox from '../widgets/ImgBox.vue'
+import imgData from '@/img-data.js'
 
+export default {
+  data(){
+    return {
+      imgData: imgData
+    }
+  },
+  components: { ImgBox }
 }
 </script>
 
-<style>
+<style scoped>
+.gallery h2 {
+  font-size: 2.2rem;
+  text-transform: uppercase;
+}
 
+.images {
+  margin-top: 30px;
+}
 </style>
---
+++ src/components/widgets/ImgBox.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="img-box col-xs-12 col-sm-6 col-lg-4" :class="image.category">
+    <b-img  :src="require(`@/assets/${image.image}`)" 
+      alt="" fluid-grow></b-img>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        image: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+.img-box img {
+  background-color: #CCC;
+  padding: 4px;
+  margin-bottom: 30px;
+}
+</style>
