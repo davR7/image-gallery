2923e68322155a20f2fb7907a8cb2b92e3c302f6
Davidson Ratis <davidsonmakarios@gmail.com>
2020-11-10 01:54:46 -0300

    created the menu component

---
+++ src/components/widgets/Menu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="menu">
+    <b-button v-for="(categ, index) in categs" :key="index" 
+      class="btn m-1" @click="categoryName(`${categ.class}`)"
+    >{{ categ.name }}</b-button>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      categs: [
+        { name: 'SmartTV', class: 'smart-tv' },
+        { name: 'Notebook', class: 'notebook' },
+        { name: 'SmartFone', class: 'mobile' },
+        { name: 'Fone de Ouvido', class: 'headphone' },
+        { name: 'Todas Imagens', class: 'all' },
+      ]
+    }
+  },
+  methods: {
+    categoryName(name){
+      this.$emit('selectedCategory', name)
+    }
+  }
+};
+</script>
+
+<style>
+</style>
