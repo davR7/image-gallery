2dc840188c6c10b1e82c9a3669f11e5d5e578eef
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-21 16:11:27 -0300

    removed - router import

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router.js'
 import './plugins/bootstrap-vue.js'
 import 'animate.css';
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
   render: h => h(App),
 }).$mount('#app')
