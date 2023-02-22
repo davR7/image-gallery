8b36f405315663a2f2df2db05bde652cad5b0cab
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-22 15:12:33 -0300

    created - ViewGallery

---
+++ src/views/ViewGallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="view-gallery">
+    <h2 class="view-gallery__title">Os 150 Pokémons</h2>
+    <CardsWrap />
+  </div>
+</template>
+
+<script>
+import CardsWrap from '../components/CardsWrap.vue';
+
+export default {
+    components: { CardsWrap }
+};
+</script>
+
+<style>
+.view-gallery__title {
+  font-size: 2rem;
+  color: #fff;
+  margin: 50px 0;
+  text-align: center;
+}
+</style>
