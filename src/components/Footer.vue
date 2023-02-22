0769968f1aca49c3ee267ed0c370a6dafffc5fb4
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-22 16:53:02 -0300

    created - Footer component

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="footer">
+    <p class="footer__text">Image Gallery &copy; 2020-2023</p>
+  </div>
+</template>
+
+<script>
+export default {
+  
+};
+</script>
+
+<style>
+.footer {
+  background-color: #ddd;
+  color: #333;
+  padding: 40px;
+  margin-top: 40px;
+}
+
+.footer__text {
+  text-align: center;
+}
+</style>
