142eed647d6f7411e9ad1e5c49de41173eb60705
Davidson Ratis <davidsonmakarios@gmail.com>
2023-02-22 10:34:58 -0300

    created - typePokemons file

---
+++ src/typePokemons.js
@@ -0,0 +1,82 @@
+export default [
+    {
+      id: 1,
+      name: "Bug",
+      class: "bug"
+    },
+    {
+      id: 2,
+      name: "Dragon",
+      class: "dragon"
+    },
+    {
+      id: 3,
+      name: "Electric",
+      class: "electric"
+    },
+    {
+      id: 4,
+      name: "Fairy",
+      class: "fairy"
+    },
+    {
+      id: 5,
+      name: "Fighting",
+      class: "fighting"
+    },
+    {
+      id: 6,
+      name: "Fire",
+      class: "fire"
+    },
+    {
+      id: 7,
+      name: "Ghost",
+      class: "ghost"
+    },
+    {
+      id: 8,
+      name: "Grass",
+      class: "grass"
+    },
+    {
+      id: 9,
+      name: "Ground",
+      class: "ground"
+    },
+    {
+      id: 10,
+      name: "Ice",
+      class: "ice"
+    },
+    {
+      id: 11,
+      name: "Normal",
+      class: "normal"
+    },
+    {
+      id: 12,
+      name: "Poison",
+      class: "poison"
+    },
+    {
+      id: 13,
+      name: "Psychic",
+      class: "psychic"
+    },
+    {
+      id: 14,
+      name: "Rock",
+      class: "rock"
+    },
+    {
+      id: 15,
+      name: "Water",
+      class: "water"
+    },
+    { 
+      id: 16,
+      name: "Todos Pokémons", 
+      class: "all" 
+    },
+  ]
